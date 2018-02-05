ffa8daddcbbe7d55dfd34c46dd1080a34904c04b
Michael Quinnemail <mquinn5@binghamton.edu>
2018-02-05 13:19:39 -0500

    created and added two files and updated makefile

---
+++ DogShelter.cpp
@@ -0,0 +1,8 @@
+#include "DogShelter.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+}
---
+++ DogShelter.h
@@ -0,0 +1,7 @@
+#ifndef MQ_DOGSHELTER_H
+#define MQ_DOGSHELTER_H
+#include <string>
+
+
+
+#endif
